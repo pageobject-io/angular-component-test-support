9d8eb11891f3ab75195612aeddc9e6cf203c173a
Akos <akos.bordas@corballis.ie>
2017-06-01 08:34:38 +0200

    release scripts updated

--- scripts/prepare-release.sh
+++ scripts/prepare-release.sh
@@ -3,10 +3,21 @@ set -exu
 
 # Clear dist/ so that we guarantee there are no stale artifacts.
 rm -rf ./dist
+rm -rf ./deploy
 
 # npm run lint
 npm run tsc
 npm run build:umd
+
+# deploy/ serves as a working directory to stage the release.
+mkdir deploy
+
+# Copy all components/ to deploy/
+cp -R ./dist/* ./deploy/
+find deploy -type d -name 'spec' -exec rm -r {} +
+cp package.json ./deploy
+
+cd ./deploy
 
 echo "Please provide the new version you want upgrade for (patch, minor, major, prepatch, preminor, premajor, prerelease):"
 read new_version
